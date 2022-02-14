c826395319a39d88d845959838ca7fc88cc8db2d
Arthurflame <artgerash@gmail.com>
2022-02-14 11:25:15 +0200

    test mods

--- solution_test.go
+++ solution_test.go
@@ -5,10 +5,9 @@ import (
 )
 
 func TestGetMessage(t *testing.T) {
-	want := string([]rune{72, 101, 108, 108, 111, 32, 128506, 65039, 32, 33})
-	got := GetMessage()
-
-	if want != got {
-		t.Errorf("GetMessage() = %v, want %v", want, got)
+	rendered := GetMessage()
+	if GetMessage() != rendered {
+		t.Error("GetMessage", rendered)
 	}
+
 }
